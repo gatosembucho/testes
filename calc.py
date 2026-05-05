cc8e6d7cbd404ca37bc2291679c007e9f4060e5c
gatosembuchoclear <malu.14geraldo@gmail.com>
2026-05-05 11:23:52 -0300

    questao 2, 3 e 4

--- calc.py
+++ calc.py
@@ -11,6 +11,21 @@ def div (a,i):
     if i == 0: 
        raise ValueError("Não é possível dividir por 0!")
     return a/i 
+
+def comp (a,i):
+    if a>i:
+        return a
+    else:
+        return i
     
-    
-    
+def imp (a):
+    if a%2==0:
+        return ("Par")
+    else:
+        return ("Impar")    
+
+def cel (a):
+    return (a-32)/1.8
+
+def fare (a):
+    return (a*1.8)+32
--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,5 @@
 import pytest
-from calc import soma, sub, div, mult
+from calc import soma, sub, div, mult, comp, imp,cel, fare
 
 def test_soma():
     assert soma(2,3) == 5
@@ -12,3 +12,15 @@ def teste_mult():
 
 def test_div():
     assert div(10,5)==2
+
+def test_comp():
+    assert comp (11,10)==11
+
+def test_imp():
+    assert imp(2)=="Par"
+
+def test_cel():
+    assert cel(32)==0
+
+def teste_fare():
+    assert fare(0)==32
